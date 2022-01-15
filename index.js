7f6c866f594c1a5022f74475cd16b545e153ae29
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-15 14:06:05 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  let answer;
+  if (number <= 250){
+    answer = 'This one is on me!';
+  } else if (number > 2000 & number < 2500) {
+      answer = 'I will gladly take your thirty bucks.';
+      }
+     else if (number > 2500) {
+      answer = 'No can do.';
+      }
+return answer;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city === 'NYC' ? "Ok, sounds good." : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(answer){
   // Write your code here!
+  let greeting;
+  switch(answer) {
+    case 'generous':
+      greeting = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      greeting = "Thank you." ;
+      break;
+    default :
+      greeting = 'Bye.' ;
+  }
+  return greeting;
 }
